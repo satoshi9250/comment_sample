5ee146c1a028951c10f361af8eda3d28cd2d03ee
satoshi9250 <67090591+satoshi9250@users.noreply.github.com>
2020-11-12 16:31:36 +0900

    master

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy
+    redirect_to("/")
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :create]
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    @comment = Comment.new
+    @like = Like.new
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/")
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
